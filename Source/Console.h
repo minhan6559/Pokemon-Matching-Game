cb33efc567f4caf8b2178dac8d929d095d4b3c8b
Hiếu Hồ <fantastichaha11@gmail.com>
2023-04-12 00:12:51 +0700

    edit cmt

--- Source/Console.h
+++ Source/Console.h
@@ -7,7 +7,6 @@ void DisableResizeWindow();
 void DisableCtrButton(bool Close, bool Min, bool Max);
 void ShowScrollbar(BOOL Show);
 
-//ref: https://codelearn.io/sharing/windowsh-ham-dinh-dang-noi-dung-console
 void SetColor(int backgound_color, int text_color);
 void GoTo(SHORT posX, SHORT posY);
 void ShowCur(bool CursorVisibility);
